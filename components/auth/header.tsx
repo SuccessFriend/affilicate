8a7a2a6de5e30ee567d97383005bcda355578337
SuccessFriend <clock971111@outlook.com>
2024-04-10 10:38:29 +0900

    test commit

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -20,7 +20,7 @@ export const Header = ({
         "text-3xl font-semibold",
         font.className,
       )}>
-        🔐 Auth
+        🔐 Sign in here
       </h1>
       <p className="text-muted-foreground text-sm">
         {label}
