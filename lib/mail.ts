51d2785b7b4524e26d4d36a18366fefdefddc752
FSPD <110429615+DeuceAceTrey@users.noreply.github.com>
2024-04-10 04:41:44 -0700

    restore mail module got resend API key

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
@@ -8,13 +8,12 @@ export const sendTwoFactorTokenEmail = async (
   email: string,
   token: string
 ) => {
-  console.log(token);
-  // await resend.emails.send({
-  //   from: "mail@auth-masterclass-tutorial.com",
-  //   to: email,
-  //   subject: "2FA Code",
-  //   html: `<p>Your 2FA code: ${token}</p>`
-  // });
+  await resend.emails.send({
+    from: "mail@auth-masterclass-tutorial.com",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code: ${token}</p>`
+  });
 };
 
 export const sendPasswordResetEmail = async (
@@ -23,12 +22,12 @@ export const sendPasswordResetEmail = async (
 ) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`
 
-  // await resend.emails.send({
-  //   from: "mail@auth-masterclass-tutorial.com",
-  //   to: email,
-  //   subject: "Reset your password",
-  //   html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
-  // });
+  await resend.emails.send({
+    from: "mail@auth-masterclass-tutorial.com",
+    to: email,
+    subject: "Reset your password",
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+  });
 };
 
 export const sendVerificationEmail = async (
@@ -37,10 +36,10 @@ export const sendVerificationEmail = async (
 ) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  // await resend.emails.send({
-  //   from: "mail@auth-masterclass-tutorial.com",
-  //   to: email,
-  //   subject: "Confirm your email",
-  //   html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
-  // });
+  await resend.emails.send({
+    from: "mail@auth-masterclass-tutorial.com",
+    to: email,
+    subject: "Confirm your email",
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
+  });
 };
