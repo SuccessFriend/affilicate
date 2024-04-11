c3383f6df1d9f7c310fa2f9c9873c1d6fe740ed8
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-11 10:05:17 +0900

    home icon

--- app/(protected)/_components/dashboard.tsx
+++ app/(protected)/_components/dashboard.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
 import { clsx } from 'clsx/lite'
 
+import { HomeIcon } from "@radix-ui/react-icons";
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 };
@@ -17,11 +18,40 @@ export const Dashboard = ({ children }: ProtectedLayoutProps) => {
 	return (
 		<>
 			<div className="bg-gray-800 text-white  h-screen w-32 left-0 top-0 z-20 fixed gap-y-2">
-				<div className="block space-y-2 mx-2 my-3">
-					<Link href="/server" className={clsx('w-full')}>Server</Link>
-					<Link href="/client" className={clsx('w-full')}>Client</Link>
-					<Link href="/admin" className={clsx('w-full')}>Admin</Link>
-					<Link href="/settings">Settings</Link>
+				<div className="my-4 mx-2">
+					<h3 className="text-xl">Affilicate</h3>
+				</div>
+				<div className="space-y-6 mx-2 my-3 [&>a]:hover:bg-gray-700">
+					<Link href="/server">
+						<div className={clsx('w-full mt-2 flex flex-row space-x-2 items-center')}>
+							<HomeIcon />
+							<div className="flex-grow">Server</div>
+						</div>
+					</Link>
+					<Link href="/client">
+						<div className={clsx('w-full mt-2 flex flex-row space-x-2 items-center')}>
+							<HomeIcon />
+							<div className="flex-grow">
+								Client
+							</div>
+						</div>
+					</Link>
+					<Link href="/admin">
+						<div className={clsx('w-full mt-2 flex flex-row space-x-2 items-center')}>
+							<HomeIcon />
+							<div className="flex-grow">
+								Admin
+							</div>
+						</div>
+					</Link>
+					<Link href="/settings">
+						<div className={clsx('w-full mt-2 flex flex-row space-x-2 items-center')}>
+							<HomeIcon />
+							<div className="flex-grow">
+								Settings
+							</div>
+						</div>
+					</Link>
 				</div>
 			</div>
 			<Navbar />
