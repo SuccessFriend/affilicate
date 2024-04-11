37098c9a5a1f86d8ffc584bff36455a368eb12fd
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-11 09:45:02 +0900

    Navbar Layout Link

--- app/(protected)/_components/dashboard.tsx
+++ app/(protected)/_components/dashboard.tsx
@@ -6,57 +6,26 @@ import { usePathname } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
+import { clsx } from 'clsx/lite'
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 };
-export const Dashboard = ({ children }: ProtectedLayoutProps) => {
-  const pathname = usePathname();
 
-    return (
-        <>
-            <div className="bg-gray-800  h-screen w-15 left-0 top-0 z-20 fixed gap-y-2">
-                <div className="block flex-row mx-2 my-3">
-                        <Button 
-                        asChild
-                        variant={pathname === "/server" ? "default" : "outline"}
-                        className="block mb-2"
-                        >
-                        <Link href="/server">
-                            Server
-                        </Link>
-                        </Button>
-                        <Button 
-                        asChild
-                        variant={pathname === "/client" ? "default" : "outline"}
-                        className="block mb-2"
-                        >
-                        <Link href="/client">
-                            Client
-                        </Link>
-                        </Button>
-                        <Button 
-                        asChild
-                        variant={pathname === "/admin" ? "default" : "outline"}
-                        className="block mb-2"
-                        >
-                        <Link href="/admin">
-                            Admin
-                        </Link>
-                        </Button>
-                        <Button 
-                        asChild
-                        variant={pathname === "/settings" ? "default" : "outline"}
-                        className="block mb-2"
-                        >
-                        <Link href="/settings">
-                            Settings
-                        </Link>
-                        </Button>
-                </div>
-            </div>
-        <Navbar />
-        {children}
+export const Dashboard = ({ children }: ProtectedLayoutProps) => {
+	const pathname = usePathname();
+	return (
+		<>
+			<div className="bg-gray-800 text-white  h-screen w-32 left-0 top-0 z-20 fixed gap-y-2">
+				<div className="block space-y-2 mx-2 my-3">
+					<Link href="/server" className={clsx('w-full')}>Server</Link>
+					<Link href="/client" className={clsx('w-full')}>Client</Link>
+					<Link href="/admin" className={clsx('w-full')}>Admin</Link>
+					<Link href="/settings">Settings</Link>
+				</div>
+			</div>
+			<Navbar />
+			{children}
     </>
   );
 };
