5e4515decc467f9341a0d907ca3450571e2935d6
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-11 20:54:52 +0900

    navbar rounded

--- app/(protected)/_components/dashboard.tsx
+++ app/(protected)/_components/dashboard.tsx
@@ -14,42 +14,48 @@ export const Dashboard = ({ children }: ProtectedLayoutProps) => {
 
   return (
     <div className="h-full w-full flex flex-row overflow-hidden">
-      <div className="bg-gray-700 text-white h-screen w-40 min-w-40 space-y-4">
+      <div className="bg-gray-700 text-white h-screen w-48 min-w-48 space-y-4">
         <div className="w-full bg-gray-800 px-2 py-4">
-          <h3 className="text-xl">Affilicate</h3>
+          <h3 className="text-xl">Affiliate</h3>
         </div>
-        <div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+        <div className="pl-2 pr-1">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/dashboard" className="flex flex-row space-x-2 items-center">
               <DashboardIcon />
               <div className="flex-grow">Dashboard</div>
             </Link>
           </div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/urls" className="flex flex-row space-x-2 items-center">
               <BarChartIcon />
               <div className="flex-grow">Affiliate URLs</div>
             </Link>
           </div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
+            <Link href="/statistics" className="flex flex-row space-x-2 items-center">
+              <BarChartIcon />
+              <div className="flex-grow">Statistics</div>
+            </Link>
+          </div>
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/server" className="flex flex-row space-x-2 items-center">
               <HomeIcon />
               <div className="flex-grow">Server</div>
             </Link>
           </div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/client" className="flex flex-row space-x-2 items-center">
               <Link2Icon />
               <div className="flex-grow">Client</div>
             </Link>
           </div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/admin" className="flex flex-row space-x-2 items-center">
               <PieChartIcon />
               <div className="flex-grow">Admin</div>
             </Link>
           </div>
-          <div className="hover:bg-gray-800 px-3 py-2">
+          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
             <Link href="/settings" className="flex flex-row space-x-2 items-center">
               <BarChartIcon />
               <div className="flex-grow">Settings</div>
