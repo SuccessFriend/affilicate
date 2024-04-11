21472b112d718a321b4bee00869e1ab1926fc24e
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-11 12:26:57 +0900

    Card 1 interface

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,6 +1,28 @@
 import { currentUser } from "@/lib/auth";
 import { UserInfo } from "@/components/user-info";
 import Card1 from '@/components/dashboard/Card1'
+import { DashboardIcon } from '@radix-ui/react-icons'
+
+const Card1Data = [
+  {
+    icon: <DashboardIcon width={30} height={30}  />,
+    title: 'Referrals',
+    amount: 9,
+    rate: 55.6
+  },
+  {
+    icon: <DashboardIcon width={30} height={30} />,
+    title: 'Referrals',
+    amount: 9,
+    rate: 55.6
+  },
+  {
+    icon: <DashboardIcon width={30} height={30} />,
+    title: 'Referrals',
+    amount: 9,
+    rate: 55.6
+  }
+]
 
 const ServerPage = async () => {
   const user = await currentUser();
@@ -11,10 +33,9 @@ const ServerPage = async () => {
       <div className="space-y-2 px-4 md:px-6">
         <div className="text-sm">Last 30 days</div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <Card1 />
-        <Card1 />
-        <Card1 />
-        <Card1 />
+          {
+            Card1Data.map((data, i) => <Card1 icon={data.icon} amount={data.amount} rate={data.rate} title={data.title} key={i} />)
+          }
       </div>
       </div>
     </div>
--- components/dashboard/Card1.tsx
+++ components/dashboard/Card1.tsx
@@ -1,16 +1,23 @@
-import { DashboardIcon } from '@radix-ui/react-icons'
+import {FC} from 'react'
 import { Button } from '@/components/ui/button'
+import { clsx } from 'clsx/lite'
+interface Card1Type {
+  icon: any;
+  title: string;
+  amount: number;
+  rate: number;
+};
 
-export default function Card1() {
+export default function Card1({ icon, title, amount, rate }: Card1Type) {
   return (
     <div className="bg-gray-50 rounded-lg overflow-hidden">
       <div className="p-4 flex flex-row w-full space-x-4 items-center">
-        <div><DashboardIcon width={30} height={30} /></div>
+        <div>{icon}</div>
         <div>
-          <p>Referrals</p>
+          <p>{title}</p>
           <div className="flex flex-row w-full space-x-2 items-end">
-            <div className="text-2xl font-bold">5.77%</div>
-            <div className="flex flex-row flex-grow items-end text-green-500 text-sm">
+            <div className="text-2xl font-bold">{ amount }</div>
+            <div className={clsx("flex flex-row flex-grow space-x-1 items-end text-green-500 text-sm", rate >= 0 ? "text-green-600": "text-red-500")}>
               <div>up</div>
               <div>9.8%</div>
             </div>
