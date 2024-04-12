e46b5f9fda0a724c69af3006e8c22b5bd633c1d6
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-12 10:01:05 +0900

    navtab data

--- app/(protected)/_components/dashboard.tsx
+++ app/(protected)/_components/dashboard.tsx
@@ -96,52 +96,12 @@ export const Dashboard = ({ children }: ProtectedLayoutProps) => {
     <div className="h-full w-full flex flex-row overflow-hidden">
       <div className="bg-gray-700 text-white h-screen w-60 min-w-60 space-y-4">
         <div className="w-full bg-gray-800 px-2 py-4">
-          <h3 className="text-xl">Affiliate</h3>
+          <h3 className="text-xl">Afsfiliate</h3>
         </div>
         <div className="pl-2 pr-1">
-          <NavTab icon={} />
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/urls" className="flex flex-row space-x-2 items-center">
-              <BarChartIcon />
-              <div className="flex-grow">Affiliate URLs</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/statistics" className="flex flex-row space-x-2 items-center">
-              <BarChartIcon />
-              <div className="flex-grow">Statistics</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/statistics" className="flex flex-row space-x-2 items-center">
-              <BarChartIcon />
-              <div className="flex-grow">Statistics</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/server" className="flex flex-row space-x-2 items-center">
-              <HomeIcon />
-              <div className="flex-grow">Server</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/client" className="flex flex-row space-x-2 items-center">
-              <Link2Icon />
-              <div className="flex-grow">Client</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/admin" className="flex flex-row space-x-2 items-center">
-              <PieChartIcon />
-              <div className="flex-grow">Admin</div>
-            </Link>
-          </div>
-          <div className="hover:bg-gray-400 pl-2 py-2 rounded-sm">
-            <Link href="/settings" className="flex flex-row space-x-2 items-center">
-              <BarChartIcon />
-              <div className="flex-grow">Settings</div>
-            </Link>
-          </div>
+          {navData.map(data => (
+            <NavTab icon={} />
+          ))}
         </div>
       </div>
       <div className="flex-grow gap-y-10 h-full overflow-hidden">
