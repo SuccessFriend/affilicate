81b82dd70a71cda1da3734cd8fd96e2d89d82c66
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-12 11:09:41 +0900

    rechart

--- app/(protected)/graphs/page.tsx
+++ app/(protected)/graphs/page.tsx
@@ -1,49 +1,55 @@
 "use client";
 
 import { currentUser } from "@/lib/auth";
-import Chart from "chart.js";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
 
 const Graphs = async () => {
   const user = await currentUser();
-
-  useEffect(() => {
-    const ctx = document.getElementById("myChart").getContext("2d");
-    new Chart(ctx, {
-      type: "line", // Change to the desired chart type (e.g., 'bar', 'doughnut', etc.)
-      data: {
-        labels: data.labels,
-        datasets: [
-          {
-            label: "Referral Earnings",
-            data: data.values,
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 2,
-            fill: false
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            display: true,
-            title: {
-              display: true,
-              text: "Month"
-            }
-          },
-          y: {
-            display: true,
-            title: {
-              display: true,
-              text: "Earnings"
-            }
-          }
-        }
-      }
-    });
-  }, [data]);
 
   return (
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
@@ -53,7 +59,27 @@ const Graphs = async () => {
         <div>Earnings</div>
 
         <div>
-          <canvas id="myChart" width="400" height="200"></canvas>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
