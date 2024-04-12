bc16b9fa1a059323a5e35d5b7120482881abdb4b
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-12 09:09:41 +0900

    CampaignTable

---
+++ components/statistics/CampaignTable.tsx
@@ -0,0 +1,54 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+
+interface ActivityType {
+  reference: number;
+  amount: number;
+  description: string;
+  status: string;
+  date: Date;
+}
+
+export default function CampaignTable({ data }: any) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Reference</TableHead>
+          <TableHead>Amount</TableHead>
+          <TableHead>description</TableHead>
+          <TableHead>status</TableHead>
+          <TableHead className="text-right">Date</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row: any, i: number) => (
+          <TableRow key={i}>
+            <TableCell className="font-medium">{row.reference}</TableCell>
+            <TableCell>{row.amount}</TableCell>
+            <TableCell>{row.description}</TableCell>
+            <TableCell>
+              <Badge variant={"success"}>{row.status}</Badge>
+            </TableCell>
+            <TableCell className="text-right">{row.date.toISOString().slice(0, 10)}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={4}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+}
--- components/statistics/Card.tsx
+++ components/statistics/Card.tsx
@@ -10,9 +10,9 @@ interface Card1Type {
 
 export default function Card({ title, amount }: Card1Type) {
   return (
-    <div className="bg-gray-50 p-4 rounded-lg overflow-hidden shadow-md">
-      <p className="font-medium">{title}</p>
-      <p className="text-2xl font-bold">{amount}</p>
+    <div className="bg-gray-50 p-6 rounded-lg overflow-hidden shadow-md">
+      <p className="text-gray-500 font-medium">{title}</p>
+      <p className="text-3xl font-bold">{amount}</p>
     </div>
   );
 }
