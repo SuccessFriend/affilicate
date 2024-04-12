9ba6677e524fc369d684158d711a106f3232b70e
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-12 09:04:44 +0900

    start

--- components/statistics/Card.tsx
+++ components/statistics/Card.tsx
@@ -11,8 +11,8 @@ interface Card1Type {
 export default function Card({ title, amount }: Card1Type) {
   return (
     <div className="bg-gray-50 p-4 rounded-lg overflow-hidden shadow-md">
-      <p>{title}</p>
-      <p>{amount}</p>
+      <p className="font-medium">{title}</p>
+      <p className="text-2xl font-bold">{amount}</p>
     </div>
   );
 }
