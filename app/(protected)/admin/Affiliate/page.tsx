978d5f59d62d4c2ce18313b8df100c0f298e0a8a
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-17 15:48:42 +0900

    affiliate dummy data

--- app/(protected)/admin/Affiliate/page.tsx
+++ app/(protected)/admin/Affiliate/page.tsx
@@ -1,4 +1,5 @@
 import { currentUser } from "@/lib/auth";
+import Table from "@/components/admin/affiliate/Table";
 
 const Creatives = async () => {
   const user = await currentUser();
@@ -7,9 +8,7 @@ const Creatives = async () => {
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
       <div className="text-2xl font-bold">admin affiliate</div>
 
-      <div>
-        <div>No admin affiliate found.</div>
-      </div>
+      <div>{}</div>
     </div>
   );
 };
