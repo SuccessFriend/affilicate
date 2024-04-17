5c3a44c880c4597046abe0a39f2ef78abb7e55ea
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-17 17:33:07 +0900

    save change

--- app/(protected)/admin/settings/page.tsx
+++ app/(protected)/admin/settings/page.tsx
@@ -9,24 +9,27 @@ const Setting = async () => {
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
       <div className="text-2xl font-bold">Settings</div>
 
-      <div>
+      <div className="space-y-8">
         <div className="grid grid-cols-4">
           <div className="col-span-1">
             <div>License key</div>
           </div>
           <div className="col-span-3">
-            <div>
+            <div className="flex flex-row space-x-2">
               <Input />
               <Button>Deactivate License</Button>
-              <span></span>
-            </div>
-            <div>
-              <Button>Lunch Setup Wizard</Button>
+              <span className="text-xs "> Your license is valid!</span>
             </div>
           </div>
         </div>
         <div className="grid grid-cols-4">
           <div className="col-span-1">Setup wizard</div>
+          <div className="col-span-3">
+            <Button>Lunch Setup Wizard</Button>
+          </div>
+        </div>
+        <div>
+          <Button>Save change</Button>
         </div>
       </div>
     </div>
