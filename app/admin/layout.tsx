6654ca9074e7cde27d46c3f9f51b24abcc1bf272
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-17 10:08:27 +0900

    user role check

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,15 @@
+"use client";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { Dashboard } from "./_components/dashboard";
+import { redirect } from "next/navigation";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 }
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
-  return <Dashboard children={children} />;
+  const user = useCurrentUser();
+  return user?.isOAuth && user.role === "ADMIN" ? <Dashboard children={children} /> : redirect("/home");
 };
 
 export default ProtectedLayout;
