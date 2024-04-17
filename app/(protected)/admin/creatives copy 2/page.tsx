a6a2ce059c55afa92940804a6309dd70f2a91728
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-17 13:51:54 +0900

    creative

---
+++ app/(protected)/admin/creatives copy 2/page.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from "@/lib/auth";
+
+const Creatives = async () => {
+  const user = await currentUser();
+
+  return (
+    <div className="space-y-10 w-full py-8 px-4 md:px-6">
+      <div className="text-2xl font-bold">admin creative</div>
+
+      <div>
+        <div>No admin creatives found.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Creatives;
