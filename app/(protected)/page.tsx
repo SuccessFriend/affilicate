7c6f68aee999fc189c47645ce19e452a34e7d740
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-17 09:34:17 +0900

    admin router

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,7 +1,8 @@
-export default Home() {
-  return (
-   <div>
-   
-   </div> 
-  )
+"use client";
+import { redirect } from "next/navigation";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export default function Page() {
+  const user = useCurrentUser();
+  user?.role === "ADMIN" ? redirect("/admin") : redirect("/home");
 }
