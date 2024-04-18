32762dbdeea2a15a400892cae65f0dcd2420b9a9
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-18 11:59:31 +0900

    Import Referrals

--- app/(protected)/admin/reports/page.tsx
+++ app/(protected)/admin/reports/page.tsx
@@ -157,6 +157,17 @@ const Creatives = async () => {
             </Button>
           </div>
         </div>
+
+        <div>
+          <h6>Import Referrals</h6>
+          <p className="text-sm !mt-6">Import a CSV of referral records.</p>
+          <Input type="file" className="w-40" />
+          <div>
+            <Button variant="outline" size="sm">
+              Import CSV
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
