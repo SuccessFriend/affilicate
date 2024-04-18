0c3c6b0246609d7020a357b5933cf5666e627b65
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-18 17:22:10 +0900

    overview accordion

--- app/(protected)/admin/overview/page.tsx
+++ app/(protected)/admin/overview/page.tsx
@@ -1,22 +1,25 @@
 import { currentUser } from "@/lib/auth";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 const Creatives = async () => {
   const user = await currentUser();
 
   return (
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
-      <div className="text-2xl font-bold">admin overview</div>
+      <div className="text-2xl font-bold">Overview</div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-6">
         <div className="flex flex-col">
-          <div>
-            <h6>Totals</h6>
+          <Accordion>
+            <AccordionTrigger>
+              <h6>Totals</h6>
+            </AccordionTrigger>
             <div className="space-y-4 p-4">
               <div>Table 1</div>
               <div>Table 2</div>
               <div>Table 3</div>
             </div>
-          </div>
+          </Accordion>
 
           <div>
             <h6>Recent Referrals</h6>
@@ -31,12 +34,17 @@ const Creatives = async () => {
 
         <div>
           <div>
-            <h6>Latest Affiliate Registrations</h6>
+            <h6>Most valuable Affiliate</h6>
             <div className="p-4">Table</div>
           </div>
 
           <div>
-            <h6>Latest Affiliate Registrations</h6>
+            <h6>Recent Referral Visits</h6>
+            <div className="p-4">Table</div>
+          </div>
+
+          <div>
+            <h6>Highest Convert URLs</h6>
             <div className="p-4">Table</div>
           </div>
         </div>
