13e0938127ad537779230931a492dbc333322c97
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-18 10:21:34 +0900

    Export Referrals

--- app/(protected)/admin/reports/page.tsx
+++ app/(protected)/admin/reports/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Select, SelectTrigger, SelectValue, SelectItem, SelectContent } from "@/components/ui/select";
 import { currentUser } from "@/lib/auth";
 
@@ -9,8 +10,8 @@ const Creatives = async () => {
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
       <div className="text-2xl font-bold">Reports</div>
 
-      <div className="space-y-6">
-        <div className="p-4 border space-y-2">
+      <div className="space-y-6 [&>div]:p-4 [&>div]:border [&>div]:space-y-2">
+        <div>
           <h6 className="font-medium">Export Affiliate</h6>
           <p className="text-sm !mt-6">Export affiliates to a CSV file.</p>
           <Select>
@@ -26,12 +27,39 @@ const Creatives = async () => {
             </SelectContent>
           </Select>
           <div>
-            <Button variant="outline">Export</Button>
+            <Button variant="outline" size="sm">
+              Export
+            </Button>
           </div>
         </div>
 
         <div>
-          <h6></h6>
+          <h6>Export Referrals</h6>
+          <p>Export referrals to a CSV file.</p>
+          <div className="space-x-4">
+            <Input placeholder="Affiliate name" />
+            <Input placeholder="Affiliate name" />
+            <Input placeholder="Affiliate name" />
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Processing" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="processing">Processing</SelectItem>
+                <SelectItem value="paid">Paid</SelectItem>
+                <SelectItem value="failed">Failed</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <p>
+            To search for an affiliate, enter the affiliate's login name, first name, or last name. Leave blank to
+            export payouts for all affiliates.
+          </p>
+          <div>
+            <Button variant="outline" size="sm">
+              Export
+            </Button>
+          </div>
         </div>
       </div>
     </div>
