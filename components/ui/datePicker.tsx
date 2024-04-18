1a3d09e0629fe472501749caedba3f19e222f9b8
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-18 15:31:55 +0900

    datePicker

--- components/ui/datePicker.tsx
+++ components/ui/datePicker.tsx
@@ -2,8 +2,7 @@
 
 import * as React from "react";
 import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-
+import { CalendarIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
