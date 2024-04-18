654669ff9d0629247bcb053d2bbb9fd6bd160ea3
DeuceAceTrey <aarontapi3@gmail.com>
2024-04-18 09:57:37 +0900

    Export Affiliate

--- app/(protected)/admin/reports/page.tsx
+++ app/(protected)/admin/reports/page.tsx
@@ -1,3 +1,4 @@
+import { Select } from "@/components/ui/select";
 import { currentUser } from "@/lib/auth";
 
 const Creatives = async () => {
@@ -5,10 +6,30 @@ const Creatives = async () => {
 
   return (
     <div className="space-y-10 w-full py-8 px-4 md:px-6">
-      <div className="text-2xl font-bold">admin reports</div>
+      <div className="text-2xl font-bold">Reports</div>
 
-      <div>
-        <div>No admin reports.</div>
+      <div className="space-y-6">
+        <div>
+          <h1>Export Affiliate</h1>
+          <p>Export affiliates to a CSV file.</p>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Today" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="today">Today</SelectItem>
+              <SelectItem value="yesterday">Yesterday</SelectItem>
+              <SelectItem value="thisweek">This Week</SelectItem>
+              <SelectItem value="lastweek">Last Week</SelectItem>
+              <SelectItem value="thismonth">This Month</SelectItem>
+              <SelectItem value="lastmonth">Last Month</SelectItem>
+              <SelectItem value="thisquarter">This Quarter</SelectItem>
+              <SelectItem value="lastquarter">Last Quarter</SelectItem>
+              <SelectItem value="thisyear">This Year</SelectItem>
+              <SelectItem value="lastyear">Last Year</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
     </div>
   );
